b4d55db8fcba900eb92bc2e748db29f93b984e22
Christian Daudt <christian.daudt@cypress.com>
2016-11-17 10:51:33 -0800

    runme update

--- runme_master.sh
+++ runme_master.sh
@@ -1,14 +1,14 @@
 docker run -it \
-  -v /boot:/boot \
-  -v /lib/modules:/lib/modules \
-  -v $PWD/fileshare:/fileshare \
-  -v /dev/bus/usb:/dev/bus/usb \
-  -v /home/csd/.ssh/id_rsa.pub:/home/lava.ssh/authorized_keys:ro \
-  --device=/dev/ttyUSB0 \
-  -p 8000:80 \
-  -p 5555:5555 \
-  -p 5556:5556 \
-  -p 2022:22 \
-  -h lava-docker \
-  --privileged \
-  lava-master $*
+ -v /boot:/boot \
+ -v /lib/modules:/lib/modules \
+ -v $PWD/fileshare:/fileshare \
+ -v /dev/bus/usb:/dev/bus/usb \
+ -v /root/.ssh/id_rsa.pub:/home/lava.ssh/authorized_keys:ro \
+ --device=/dev/ttyUSB0 \
+ -p 8000:80 \
+ -p 5555:5555 \
+ -p 5556:5556 \
+ -p 2022:22 \
+ -h lava-docker \
+ --privileged \
+ rodan.ric.broadcom.com:5000/lava/master:2016-11.1
