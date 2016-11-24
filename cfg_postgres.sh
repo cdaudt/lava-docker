1333e1e0933528b3da31068f446ba6430cbff6d7
Christian Daudt <christian.daudt@cypress.com>
2016-11-24 14:59:42 -0800

    remove dependency on postgres user in cfg_postgres

--- cfg_postgres.sh
+++ cfg_postgres.sh
@@ -18,6 +18,10 @@ set -e
 # the debian-policy package
 
 . /etc/lava-server/instance.conf
+export PGHOST=$LAVA_DB_SERVER
+export PGPASSWORD=lava123
+export PGUSER=postgres
+export PGPORT=$LAVA_DB_PORT
 
 install_database()
 {
@@ -25,8 +29,8 @@ install_database()
     pg=1
     limit=3
     while [ $pg -le $limit ]; do
-        if ! pg_isready -d '$LAVA_DB_NAME' -p $LAVA_DB_PORT -q; then
-            echo "[$pg] Postgres not ready for connection to $LAVA_DB_NAME on port $LAVA_DB_PORT."
+        if ! pg_isready -q; then
+            echo "[$pg] Postgres not ready for connection to $LAVA_DB_NAME on port $LAVA_DB_PORT.PGHOST=${PGHOST}"
             sleep 1
         else
             break
@@ -38,24 +42,24 @@ install_database()
         exit $pg
     fi
     # Create database user, if it doesn't exist
-    if ! su postgres -c "psql \"-c SELECT usename FROM pg_user WHERE usename='$LAVA_DB_USER'\"" | grep "$LAVA_DB_USER"; then
-        su postgres -c "createuser --no-createdb --encrypted --login --no-superuser --no-createrole --no-password --port $LAVA_DB_PORT $LAVA_DB_USER"|| "Failed to create database user"
-        # Set a password for our new user
-        su postgres -c "psql --port $LAVA_DB_PORT --quiet --command=\"ALTER USER \"$LAVA_DB_USER\" WITH PASSWORD '$LAVA_DB_PASSWORD'\"" || die "Failed to set database password"
+    if ! psql -c "SELECT usename FROM pg_user WHERE usename='$LAVA_DB_USER'" | grep "$LAVA_DB_USER"; then
+        echo "Creating user:${LAVA_DB_USER}"
+        psql --command="CREATE USER $LAVA_DB_USER WITH NOSUPERUSER NOCREATEDB LOGIN NOCREATEROLE ENCRYPTED PASSWORD '$LAVA_DB_PASSWORD'" || "Failed to create database user"
     fi
     # Create a database for our new user, if it doesn't exist
-    if ! su postgres -c "psql -c \"SELECT datname FROM pg_database WHERE datname='$LAVA_DB_NAME'\"" | grep "$LAVA_DB_NAME"; then
-        su postgres -c "createdb --port $LAVA_DB_PORT --locale=C.UTF-8 --encoding=UTF-8 --owner=$LAVA_DB_USER --template=template0 --no-password $LAVA_DB_NAME" || die "Failed to create a database"
+    if ! psql -c "SELECT datname FROM pg_database WHERE datname='$LAVA_DB_NAME'" | grep "$LAVA_DB_NAME"; then
+        echo "Creating DB:${LAVA_DB_NAME}"
+        psql --command="CREATE DATABASE $LAVA_DB_NAME WITH ENCODING=UTF8 OWNER=$LAVA_DB_USER TEMPLATE=template0" || "Failed to create a database"
     fi
     # Create devel user, if it doesn't exist, for unit test support
-    if ! su postgres -c "psql \"-c SELECT usename FROM pg_user WHERE usename='devel'\"" | grep "devel"; then
-        su postgres -c "createuser --createdb --login --no-superuser --no-createrole --no-password --port $LAVA_DB_PORT devel"|| "Failed to create test case user"
-        # Set a password for the devel user
-        su postgres -c "psql --port $LAVA_DB_PORT --quiet --command=\"ALTER USER \"devel\" WITH PASSWORD 'devel'\"" || die "Failed to set test case password"
+    if ! psql --command="SELECT usename FROM pg_user WHERE usename='devel'" | grep "devel"; then
+        echo "Creating user:devel"
+        psql --command="CREATE USER devel WITH NOSUPERUSER CREATEDB LOGIN NOCREATEROLE PASSWORD 'devel'" || "Failed to create test user"
     fi
     # create the devel database, if it doesn't exist
-    if ! su postgres -c "psql -c \"SELECT datname FROM pg_database WHERE datname='devel'\"" | grep "devel"; then
-        su postgres -c "createdb devel --owner devel"
+    if ! psql --command="SELECT datname FROM pg_database WHERE datname='devel'" | grep "devel"; then
+        echo "Creating DB:devel"
+        psql --command="CREATE DATABASE devel WITH OWNER=devel" || die "Failed to create a database"
     fi
     # syncdb
     # fake-initial only in django 1.8 but needed for upgrades from 1.7 using 1.9
@@ -66,8 +70,8 @@ install_database()
     fi
     lava-server manage refresh_queries --all
     # superuser - password must be changed
-    if ! su postgres -c "psql \"-c SELECT usename FROM pg_user WHERE usename='$LAVA_SYS_USER'\"" | grep "$LAVA_SYS_USER"; then
-        if ! su postgres -c "psql $LAVA_DB_NAME \"-c SELECT username FROM auth_user WHERE username='$LAVA_SYS_USER'\"" | grep "$LAVA_SYS_USER"; then
+    if ! psql --command="SELECT usename FROM pg_user WHERE usename='$LAVA_SYS_USER'" | grep "$LAVA_SYS_USER"; then
+        if ! psql --command="SELECT username FROM auth_user WHERE username='$LAVA_SYS_USER'" | grep "$LAVA_SYS_USER"; then
             lava-server manage createsuperuser --noinput --username=$LAVA_SYS_USER --email=$LAVA_SYS_USER@lava.invalid || true
         fi
     fi
