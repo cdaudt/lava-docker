bf730ceb5404b36f31d64a15fee308a1362d5690
Christian Daudt <christian.daudt@cypress.com>
2016-12-01 14:34:05 -0800

    device adding now done via config file

--- add-devices-to-lava.sh
+++ add-devices-to-lava.sh
@@ -28,7 +28,4 @@ mkdir -p /etc/dispatcher-config/devices
 devicename=943907AEVAL1F-1
 devicetype=943907AEVAL1F
 ## Add device
-csrf="csrfmiddlewaretoken="$(cat  ${tools_path}/cookies.txt | grep csrftoken | cut -d$'\t' -f 7)
-createdevice=$csrf\&hostname=$devicename\&device_type=$devicetype\&device_version=1\&status=1\&health_status=0\&is_pipeline="on"\&worker_host=${WORKER}
-curl -b ${tools_path}/cookies.txt -c ${tools_path}/cookies.txt -d $createdevice -X POST $lavaurl/admin/lava_scheduler_app/device/add/
 lava-server manage device-dictionary --hostname $devicename --import /etc/dispatcher-config/devices/$devicename.jinja2
