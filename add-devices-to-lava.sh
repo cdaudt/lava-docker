4efb67429d5af344f34fbb3ed2bc7a9c2590aace
Christian Daudt <christian.daudt@cypress.com>
2016-12-01 15:13:06 -0800

    clear out add-devices

--- add-devices-to-lava.sh
+++ add-devices-to-lava.sh
@@ -10,21 +10,6 @@ fi
 
 lavaurl=http://localhost
 tools_path="${tools_path:-/home/lava/bin}"
-hostn=$(hostname)
 
-#obtain the csrf token
-data=$(curl -s -c ${tools_path}/cookies.txt $lavaurl/accounts/login/); tail ${tools_path}/cookies.txt
-
-#login
-csrf="csrfmiddlewaretoken="$(grep csrftoken ${tools_path}/cookies.txt | cut -d$'\t' -f 7); echo "$csrf"
-login=$csrf\&username=$adminuser\&password=$adminpass; echo $login
-curl -b ${tools_path}/cookies.txt -c ${tools_path}/cookies.txt -d $login -X POST $lavaurl/admin/login/
-
-#create workers
+#create workers, devices, devicetypes
 ${tools_path}/setup_server.py --url ${lavaurl} /fileshare/cfg/server.json
-
-mkdir -p /etc/dispatcher-config/devices
-
-devicename=943907AEVAL1F-1
-devicetype=943907AEVAL1F
-## Add device
