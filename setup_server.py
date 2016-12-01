0c5860966a9cb0286337836de553d2976b211ee7
Christian Daudt <christian.daudt@cypress.com>
2016-12-01 11:39:17 -0800

    add_device implemented in setup_server.py

--- setup_server.py
+++ setup_server.py
@@ -90,6 +90,51 @@ def add_devicetype(server, dt):
     #print "DT:result= {} cookies= {}".format(dt, dt.cookies);
     #print "DT:text={}".format(dt.text);
 
+def add_device(server, dev):
+    # Sequence is:
+    # GET /accounts/login -> cookies1
+    # POST /admin/login(cookies1,username,password) -> cookies2
+    # POST /admin/lava_scheduler_app/device/add(cookies2,device)
+    print("Adding device:{} to URL:{}".
+            format(dev['name'], server['url']))
+    if server['url'] == None:
+        raise Exception("Need URL")
+
+    resp = requests.get(urlparse.urljoin(server['url'], '/accounts/login/'))
+    #print "Get Cookies: result= {} cookies= {}".format(resp, resp.cookies);
+    #print "Get Cookies:text={}".format(resp.text);
+
+    auth = {
+     'csrfmiddlewaretoken': resp.cookies['csrftoken'],
+     'username': server['username'],
+     'password': server['password']
+    }
+
+    login = requests.post(urlparse.urljoin(server['url'], '/admin/login/'),
+                            data=auth,
+                            cookies=resp.cookies,
+                            allow_redirects=False)
+    #print "Login:result= {} cookies= {}".format(login, login.cookies.keys);
+    #print "Login:text={}".format(login.text);
+
+    dev_req = {
+     'csrfmiddlewaretoken': login.cookies['csrftoken'],
+     'hostname': dev['name'],
+     'device_type': dev['devicetype'],
+     'device_version': '1',
+     'status': '1',
+     'health_status': '0',
+     'is_pipeline': 'on',
+     'worker_host': dev['worker']
+    }
+
+    dev = requests.post(urlparse.urljoin(server['url'],
+                                '/admin/lava_scheduler_app/device/add/'),
+                            data=dev_req,
+                            cookies=login.cookies)
+    #print "Dev:result= {} cookies= {}".format(dev, dev.cookies);
+    #print "Dev:text={}".format(dev.text);
+
 
 def myargs(argv):
   parser = OptionParser()
@@ -119,6 +164,10 @@ def main(argv):
   if 'devicetypes' in ini:
       for dt in ini['devicetypes']:
             add_devicetype(server, dt)
+
+  if 'devices' in ini:
+      for dev in ini['devices']:
+            add_device(server, dev)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
